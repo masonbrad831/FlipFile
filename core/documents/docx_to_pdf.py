679234811ac0d3f013139d72eb39b98dcb227462
Mason Bradford <masonbrad831@gmail.com>
2026-02-07 20:25:39 -0700

    Update docx_to_pdf.py
    remove print functions for debugging

--- core/documents/docx_to_pdf.py
+++ core/documents/docx_to_pdf.py
@@ -6,14 +6,12 @@ import subprocess
 def convert_docx_to_pdf(docxFilePath):
     #Grab the file name
     fileName = Path((docxFilePath.split("/"))[-1]).stem
-    print(fileName)
 
     #get temp folder location
     scriptDir = Path(__file__).parent
     outputDir = (scriptDir / "../../temp").resolve()
     outputDir.mkdir(parents=True, exist_ok=True)
     completePdfFilePath = outputDir / f"{fileName}.pdf"
-    print(completePdfFilePath)
 
     try:
         subprocess.run([
