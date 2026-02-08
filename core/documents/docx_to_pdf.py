4d83ab5f16a81bde1d0c590b8993e530194497dc
Mason Bradford <masonbrad831@gmail.com>
2026-02-07 20:21:43 -0700

    Create docx_to_pdf.py

---
+++ core/documents/docx_to_pdf.py
@@ -0,0 +1,26 @@
+from pathlib import Path
+import subprocess
+
+
+
+def convert_docx_to_pdf(docxFilePath):
+    #Grab the file name
+    fileName = Path((docxFilePath.split("/"))[-1]).stem
+    print(fileName)
+
+    #get temp folder location
+    scriptDir = Path(__file__).parent
+    outputDir = (scriptDir / "../../temp").resolve()
+    outputDir.mkdir(parents=True, exist_ok=True)
+    completePdfFilePath = outputDir / f"{fileName}.pdf"
+    print(completePdfFilePath)
+
+    subprocess.run([
+        "soffice.exe", "--headless",
+        "--convert-to", "pdf",
+        str(docxFilePath),
+        "--outdir", str(Path(completePdfFilePath).parent)
+    ], check=True)
+
+
+
